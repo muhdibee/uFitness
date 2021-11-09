392c2091f681b63740725f688299cb91d5c127cc
Muhammad Ibrahim <muhdibee@gmail.com>
2021-11-09 09:44:24 +0100

    Created Redux store

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Text, View,} from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reduders'
 import AddEntry from './components/AddEntry';
 
 const App = () => {
+  const store = createStore(reducer);
   return (
-    <View>
-      <AddEntry />
-      <Text>Base View</Text>
-    </View>
+    <Provider store={store}>
+        <View>
+          <AddEntry />
+          <Text>Base View</Text>
+        </View>
+    </Provider>
   );
 };
 
